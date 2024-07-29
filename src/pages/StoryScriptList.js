f2f952f49f63d180279475dcf0e32c88e8be3ea4
divtech_admin <kurangwahyu@gmail.com>
2024-07-29 16:02:52 +0700

    push

--- src/pages/StoryScriptList.js
+++ src/pages/StoryScriptList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DataTable from "react-data-table-component";
-import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { FaTrash, FaEdit, FaFilePdf } from "react-icons/fa";
+import { Button, OverlayTrigger, Tooltip, Modal } from "react-bootstrap";
+import { FaTrashAlt, FaEdit, FaFilePdf } from "react-icons/fa";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,6 +15,8 @@ const renderTooltip = (props) => (
 const StoryScriptList = ({ scripts, deleteScript, updateScript }) => {
   const [editRowId, setEditRowId] = useState(null);
   const [editData, setEditData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
 
   const handleEditClick = (row, rowIndex) => {
     setEditRowId(rowIndex);
@@ -34,23 +36,59 @@ const StoryScriptList = ({ scripts, deleteScript, updateScript }) => {
     setEditRowId(null);
     setEditData({});
   };
-
   const exportToPdf = () => {
     const doc = new jsPDF();
     doc.text("Daftar Script Cerita", 14, 16);
+
+    // Define table columns and data
+    const columns = ["No", "Judul", "Konten", "Gambar"];
+    const data = scripts.map((script, index) => [
+      index + 1,
+      script.title,
+      script.content,
+      script.image
+        ? { image: script.image, width: 40, height: 30 }
+        : "Tidak ada gambar",
+    ]);
+
+    // Add the table to the PDF
     doc.autoTable({
-      head: [["No", "Judul", "Konten"]],
-      body: scripts.map((script, index) => [
-        index + 1,
-        script.title,
-        script.content,
-      ]),
+      head: [columns],
+      body: data,
       startY: 20,
+      theme: "grid", // or 'striped', 'plain', etc.
       styles: {
         fontSize: 10,
+        cellPadding: 4,
+      },
+      columnStyles: {
+        0: { halign: "center" },
+        1: { valign: "middle" },
+        2: { valign: "middle", cellWidth: "wrap" },
+        3: { valign: "middle", cellWidth: 40 }, // Adjust width for images
+      },
+      didDrawCell: (data) => {
+        if (data.column.index === 3 && data.cell.raw.image) {
+          // Add image to the PDF
+          doc.addImage(
+            data.cell.raw.image,
+            "JPEG",
+            data.cell.x + 1,
+            data.cell.y + 1,
+            40,
+            30
+          );
+        }
       },
     });
+
+    // Save the PDF
     doc.save("story_scripts.pdf");
+  };
+
+  const handleImageClick = (imageSrc) => {
+    setModalImage(imageSrc);
+    setShowModal(true);
   };
 
   const columns = [
@@ -105,173 +143,129 @@ const StoryScriptList = ({ scripts, deleteScript, updateScript }) => {
       },
     },
     {
+      name: "Gambar",
+      selector: (row) =>
+        row.image ? (
+          <img
+            src={row.image}
+            alt="Preview"
+            style={{
+              maxWidth: "100px",
+              maxHeight: "50px",
+              objectFit: "cover",
+              cursor: "pointer",
+            }}
+            onClick={() => handleImageClick(row.image)}
+          />
+        ) : (
+          "Tidak ada gambar"
+        ),
+      center: true,
+      width: "120px",
+    },
+    {
       name: "Aksi",
       cell: (row, index) =>
         editRowId === index ? (
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <>
             <Button
               variant="success"
-              size="sm"
               onClick={handleSave}
-              style={{
-                backgroundColor: "#28a745",
-                borderColor: "#28a745",
-                boxShadow: "0 2px 6px rgba(40, 167, 69, 0.5)",
-                marginRight: "5px",
-              }}
+              style={{ marginRight: "5px" }}
             >
               Simpan
             </Button>
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={handleCancel}
-              style={{
-                backgroundColor: "#6c757d",
-                borderColor: "#6c757d",
-                boxShadow: "0 2px 6px rgba(108, 117, 125, 0.5)",
-              }}
-            >
+            <Button variant="secondary" onClick={handleCancel}>
               Batal
             </Button>
-          </div>
+          </>
         ) : (
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <>
             <OverlayTrigger
               placement="top"
-              overlay={renderTooltip({ children: "Edit" })}
+              overlay={renderTooltip({ children: <FaEdit /> })}
             >
               <Button
                 variant="warning"
-                size="sm"
                 onClick={() => handleEditClick(row, index)}
-                style={{
-                  backgroundColor: "#ffc107",
-                  borderColor: "#ffc107",
-                  boxShadow: "0 2px 6px rgba(255, 193, 7, 0.5)",
-                  marginRight: "5px",
-                }}
+                style={{ marginRight: "5px" }}
               >
                 <FaEdit />
               </Button>
             </OverlayTrigger>
             <OverlayTrigger
               placement="top"
-              overlay={renderTooltip({ children: "Hapus" })}
+              overlay={renderTooltip({ children: <FaTrashAlt /> })}
             >
               <Button
                 variant="danger"
-                size="sm"
                 onClick={() => deleteScript(index)}
-                style={{
-                  backgroundColor: "#dc3545",
-                  borderColor: "#dc3545",
-                  boxShadow: "0 2px 6px rgba(220, 53, 69, 0.5)",
-                }}
+                style={{ marginRight: "5px" }}
               >
-                <FaTrash />
+                <FaTrashAlt />
               </Button>
             </OverlayTrigger>
-          </div>
+          </>
         ),
-      center: true,
-      width: "160px",
+      width: "150px",
     },
   ];
 
   return (
     <div
       style={{
-        padding: "15px",
-        maxWidth: "90%",
+        padding: "20px",
+        maxWidth: "1200px",
         margin: "20px auto",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
         backgroundColor: "#ffffff",
-        borderRadius: "10px",
-        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-        transition: "all 0.3s ease-in-out",
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "20px",
-        }}
-      >
-        <h2
-          style={{
-            textAlign: "center",
-            color: "#1baa75",
-            fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-            fontWeight: "700",
-          }}
-        >
-          Daftar Script Cerita
-        </h2>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Button
           variant="primary"
           onClick={exportToPdf}
-          style={{
-            backgroundColor: "#007bff",
-            borderColor: "#007bff",
-            boxShadow: "0 2px 6px rgba(0, 123, 255, 0.5)",
-          }}
+          style={{ marginBottom: "10px" }}
         >
-          <FaFilePdf /> Export ke PDF
+          Export to PDF
         </Button>
       </div>
       <DataTable
         columns={columns}
         data={scripts}
+        noDataComponent="Tidak ada data"
         pagination
+        striped
         highlightOnHover
         responsive
         dense
-        noDataComponent="Tidak ada data yang tersedia"
-        customStyles={{
-          rows: {
-            style: {
-              minHeight: "50px",
-              "&:hover": {
-                backgroundColor: "#f1f3f5",
-                cursor: "pointer",
-              },
-            },
-          },
-          headCells: {
-            style: {
-              backgroundColor: "#e9ecef",
-              fontSize: "15px",
-              fontWeight: "bold",
-              color: "#495057",
-              borderTop: "2px solid #dee2e6",
-              borderBottom: "2px solid #dee2e6",
-              paddingLeft: "10px", // Menambahkan padding kiri
-              paddingRight: "10px", // Menambahkan padding kanan
-            },
-          },
-          cells: {
-            style: {
-              borderBottom: "2px solid #dee2e6",
-              borderRight: "2px solid #dee2e6",
-              paddingLeft: "10px", // Menambahkan padding kiri
-              paddingRight: "10px", // Menambahkan padding kanan
-              "&:last-of-type": {
-                borderRight: "none",
-              },
-              transition: "background-color 0.3s ease-in-out",
-            },
-          },
-          table: {
-            style: {
-              border: "2px solid #dee2e6",
-              borderRadius: "10px",
-              overflow: "hidden",
-            },
-          },
+        theme="solarized"
+        style={{
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
         }}
       />
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Ilustrasi Animasi</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={modalImage}
+            alt="Larger preview"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "500px",
+              objectFit: "contain",
+            }}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Tutup
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
